141d687aef8c883bb370fc9d81744a8b9e647d11
Markus Hedlund <znarkus@gmail.com>
2015-05-29 22:32:53 +0200

    Connected frontend to backend

--- generator.js
+++ generator.js
@@ -1,83 +1,104 @@
+'use strict';
+
 var libxmljs = require('libxmljs');
-var fs = require('fs');
-//var rtfjs = require('rtf.js');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 var _ = require('lodash');
-var content = [
-	{ title: 'GUDS KRAFT I DITT LIV' },
-	{
-		reference: 'APOSTLAGÄRNINGARNA 2:1-4 (SFB)',
-		verse: '1 När pingstdagen hade kommit var de alla samlade. 2 Då kom plötsligt från himlen ett dån, som när en våldsam storm drar fram, och det fyllde hela huset där de satt.'
-	},
-	{
-		reference: 'APOSTLAGÄRNINGARNA 2:1-4 (SFB)',
-		verse: '3 Tungor som av eld visade sig för dem och fördelade sig och satte sig på var och av dem.'
-	}
-];
+var base = require('base-converter');
+var moment = require('moment');
+var debug = require('debug')('app');
+//var rtfjs = require('rtf.js');
+//var content = [
+//	{ title: 'GUDS KRAFT I DITT LIV' },
+//	{
+//		reference: 'APOSTLAGÄRNINGARNA 2:1-4 (SFB)',
+//		verse: '1 När pingstdagen hade kommit var de alla samlade. 2 Då kom plötsligt från himlen ett dån, som när en våldsam storm drar fram, och det fyllde hela huset där de satt.'
+//	},
+//	{
+//		reference: 'APOSTLAGÄRNINGARNA 2:1-4 (SFB)',
+//		verse: '3 Tungor som av eld visade sig för dem och fördelade sig och satte sig på var och av dem.'
+//	}
+//];
 
-fs.readFile('preach.pro5', function(e, xml) {
-	var xmlDoc = libxmljs.parseXml(xml);
-	var titleSlide;
-	var verseSlide;
-	var lastSlide;
+exports.generate = function(content, outputDir) {
+	var outputPath = outputDir
+		+ moment().format('YYMMDD-')
+		+ base.decTo62(new Date().getTime()) + '.pro5';
 
-	//xmlDoc.find('//RVTextElement').forEach(function(textElement) {
-	//	var attr = textElement.attr('RTFData');
-	//	var rtfData = attr.value();
-	//	var buffer = new Buffer(rtfData, 'base64');
-	//	var string = buffer.toString('ascii');
+	return fs.readFileAsync('template/preach.pro5')
+		.then(function(xml) {
+			var xmlDoc = libxmljs.parseXml(xml);
+			var titleSlide;
+			var verseSlide;
+			//var lastSlide;
 
-	forEachSlideTexts(xmlDoc, function(textElement, attr, string) {
-		if (string.indexOf('BIBLEREFERENCE') > -1) {
-			verseSlide = findParent(xmlDoc, 'RVDisplaySlide', textElement);
-		} else if (string.indexOf('PREACHTITLE') > -1) {
-			titleSlide = findParent(xmlDoc, 'RVDisplaySlide', textElement);
-		}
-	});
+			//xmlDoc.find('//RVTextElement').forEach(function(textElement) {
+			//	var attr = textElement.attr('RTFData');
+			//	var rtfData = attr.value();
+			//	var buffer = new Buffer(rtfData, 'base64');
+			//	var string = buffer.toString('ascii');
 
-	lastSlide = verseSlide;
-
-	_.forEach(content.reverse(), function(node) {
-		var slide;
-
-		if (node.title) {
-			slide = titleSlide.clone();
-
-			forEachSlideTexts(slide, function(textElement, attr, string) {
-				string = string.replace('PREACHTITLE', convertUnicode(node.title));
-				attr.value(new Buffer(string).toString('base64'));
+			forEachSlideTexts(xmlDoc, function(textElement, attr, string) {
+				if (string.indexOf('BIBLEREFERENCE') > -1) {
+					verseSlide = findParent(xmlDoc, 'RVDisplaySlide', textElement);
+				} else if (string.indexOf('PREACHTITLE') > -1) {
+					titleSlide = findParent(xmlDoc, 'RVDisplaySlide', textElement);
+				}
 			});
-		} else {
-			slide = verseSlide.clone();
 
-			forEachSlideTexts(slide, function(textElement, attr, string) {
-				string = string.replace('BIBLEVERSE', convertUnicode(node.verse));
-				string = string.replace('BIBLEREFERENCE', convertUnicode(node.reference));
-				attr.value(new Buffer(string).toString('base64'));
+			//lastSlide = verseSlide;
+
+			_.forEach(content.reverse(), function(node) {
+				var slide;
+
+				debug(node);
+
+				if (node.title) {
+					slide = titleSlide.clone();
+
+					forEachSlideTexts(slide, function(textElement, attr, string) {
+						string = string.replace('PREACHTITLE', convertUnicode(node.title));
+						attr.value(new Buffer(string).toString('base64'));
+					});
+				} else {
+					slide = verseSlide.clone();
+
+					forEachSlideTexts(slide, function(textElement, attr, string) {
+						debug('Text element: %s', string);
+						string = string.replace('BIBLEVERSE', node.verse.text);
+						string = string.replace('VERSENUMBER', node.verse.number ? node.verse.number : '');
+						string = string.replace('BIBLEREFERENCE', node.reference);
+						attr.value(new Buffer(convertUnicode(string)).toString('base64'));
+					});
+				}
+
+				var all = xmlDoc.find('//RVDisplaySlide');
+				all[all.length - 1].addPrevSibling(slide);
+				//lastSlide = slide;
 			});
-		}
 
-		var all = xmlDoc.find('//RVDisplaySlide');
-		all[all.length - 1].addPrevSibling(slide);
-		//lastSlide = slide;
-	});
-
-	/*xmlDoc.find('//RVTextElement').forEach(function(textElement) {
-		var attr = textElement.attr('RTFData');
-		var rtfData = attr.value();
-		var buffer = new Buffer(rtfData, 'base64');
-		var string = buffer.toString('ascii');
+			/*xmlDoc.find('//RVTextElement').forEach(function(textElement) {
+			 var attr = textElement.attr('RTFData');
+			 var rtfData = attr.value();
+			 var buffer = new Buffer(rtfData, 'base64');
+			 var string = buffer.toString('ascii');
 
 
-		_.forEach(content, function(text, key) {
-			string = string.replace(key, convertUnicode(text));
+			 _.forEach(content, function(text, key) {
+			 string = string.replace(key, convertUnicode(text));
+			 });
+
+			 console.log(string);
+			 attr.value(new Buffer(string).toString('base64'));
+			 });*/
+
+			return fs.writeFile(outputPath, xmlDoc.toString());
+		})
+		.then(function() {
+			return outputPath;
 		});
+};
 
-		console.log(string);
-		attr.value(new Buffer(string).toString('base64'));
-	});*/
-
-	fs.writeFile('generated.pro5', xmlDoc.toString());
-});
 
 //function cloneSlide(slide) {
 //	slide.addNextSibling(slide.clone());
@@ -106,11 +127,14 @@ function convertUnicode(string) {
 		//console.log(char, charCode);
 
 		if (charCode > 127) {
-			char = '\\\'' + charCode.toString(16);
+			char = '\\u' + charCode + '?';
+			//char = '\\\'' + charCode.toString(16);
 		}
 
 		newString += char;
 	});
+
+	debug('Converted unicode string: %s', newString);
 
 	return newString;
 }
--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+var generate = require('./generator').generate;
 var app = express();
 
+app.use(bodyParser.json());
 app.use(express.static('www'));
+app.use('/generated', express.static('generated'));
 app.use('/lib', express.static('bower_components'));
+
+app.post('/download', function(req, res) {
+	generate(req.body, 'generated/')
+		.then(function(path) {
+			res.send({ url: '/' + path });
+		});
+});
 
 var server = app.listen(3000, function () {
 
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Server listening at http://%s:%s', host, port);
 });
